9629a433318d92f75d27da8199b494f0be793d2a
Rafael <rarorza@proton.me>
2023-07-08 23:26:27 -0300

    Add media query

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 
-const titulo = document.querySelector(".digitando");
-function digitando(elemento) {
-    const arrayTitulo = elemento.innerHTML.split('');
-    elemento.innerHTML = '';
-    arrayTitulo.forEach((letra, i)=>{
-        setTimeout(()=>{
-            elemento.innerHTML += letra;
-        }, 75 * i)
-    })
+function typing(){
+    function digitando(elemento) {
+        const arrayTitulo = elemento.innerHTML.split('');
+        elemento.innerHTML = '';
+        arrayTitulo.forEach((letra, i)=>{
+            setTimeout(()=>{
+                elemento.innerHTML += letra;
+            }, 75 * i)
+        })
+    }
+    const titulo = document.querySelector(".digitando");
+    digitando(titulo);
 }
 
-digitando(titulo);
+const mobileMenu = document.querySelector('.fa-bars');
+
+mobileMenu.addEventListener('click'), ()=>{
+    mobileMenu.classList.toggle('fa-x')
+}
+
+typing()
