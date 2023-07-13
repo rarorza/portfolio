4fcb3b30efced124b6b9853e9868bc73ac40ef5e
Rafael <rarorza@proton.me>
2023-07-13 18:10:55 -0300

    add responsiveness in resume section

--- script.js
+++ script.js
@@ -1,21 +1,75 @@
-const titulo = document.querySelector(".digitando");
-const mobileMenu = document.querySelector(".fa-bars");
-const navMenu = document.querySelector('header .navegacao-primaria')
 
-function digitando(elemento) {
+function digitandoTitulo() {
+  const titulo = document.querySelector(".digitando");
+
+  function letra(elemento) {
     const arrayTitulo = elemento.innerHTML.split('');
     elemento.innerHTML = '';
-    arrayTitulo.forEach((letra, i)=>{
-        setTimeout(()=>{
-            elemento.innerHTML += letra;
-        }, 75 * i)
+    arrayTitulo.forEach((letra, i) => {
+      setTimeout(() => {
+        elemento.innerHTML += letra;
+      }, 75 * i)
     })
+  }
+  letra(titulo);
 }
-digitando(titulo);
 
-mobileMenu.addEventListener('click', ()=>{
+function menuMobile() {
+  const mobileMenu = document.querySelector(".fa-bars");
+  const navMenu = document.querySelector('header .navegacao-primaria')
+  
+  mobileMenu.addEventListener('click', () => {
     mobileMenu.classList.toggle('fa-x')
     navMenu.classList.toggle('ativado')
-})
+  })
+}
 
-typing()
+function sobreMim() {
+  const divExperience = document.querySelectorAll('.experience_content div');
+  const liExperience = document.querySelectorAll('.experience_content ul li');
+  const divEducation = document.querySelectorAll('.education_content div');
+  const liEducation = document.querySelectorAll('.education_content ul li');
+  
+  divExperience[0].classList.add('ativo');
+  liExperience[0].classList.add('ativo');
+  divEducation[0].classList.add('ativo');
+  liEducation[0].classList.add('ativo');
+
+  function slideShowExp(index) {
+    divExperience.forEach((div)=>{
+      div.classList.remove('ativo');
+    });
+    liExperience.forEach((button)=>{
+      button.classList.remove('ativo');
+    });
+    divExperience[index].classList.add('ativo');
+    liExperience[index].classList.add('ativo');
+  }
+  
+  function slideShowEdu(index) {
+    divEducation.forEach((div)=>{
+      div.classList.remove('ativo');
+    });
+    liEducation.forEach((button)=>{
+      button.classList.remove('ativo');
+    });
+    divEducation[index].classList.add('ativo');
+    liEducation[index].classList.add('ativo');
+  }
+  
+  liExperience.forEach((event,index)=>{
+    event.addEventListener('click', ()=>{
+      slideShowExp(index);
+    });
+  });
+  
+  liEducation.forEach((event, index)=>{
+    event.addEventListener('click', ()=>{
+      slideShowEdu(index);
+    });
+  });
+}
+
+digitandoTitulo();
+menuMobile();
+sobreMim();
