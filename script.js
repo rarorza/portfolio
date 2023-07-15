67b958d0edb2c208828fc84ddf20aca16a0a2a9c
Rafael <rarorza@proton.me>
2023-07-15 00:45:19 -0300

    Responsiveness modification

--- script.js
+++ script.js
@@ -69,71 +69,74 @@ function sobreMim() {
   });
 }
 
-const listaAll = document.querySelectorAll('.projects_storage ul li');
-const buttonGeral = document.querySelectorAll('.projects_models ul li');
-const buttonAll = document.querySelectorAll('.projects_models .all');
-
-function removeClick(index) {
+function gridProjects(){
+  const listaAll = document.querySelectorAll('.projects_storage ul li');
+  const buttonGeral = document.querySelectorAll('.projects_models ul li');
+  const buttonAll = document.querySelectorAll('.projects_models .all');
+  
+  function removeClick(index) {
+    buttonGeral.forEach((item) => {
+      item.classList.remove('ativo');
+    });
+    buttonGeral[index].classList.add('ativo');
+  }
+  
+  buttonGeral.forEach((event, index) => {
+    event.addEventListener('click', () => {
+      removeClick(index);
+    });
+  });
+  
   buttonGeral.forEach((item) => {
-    item.classList.remove('ativo');
+    item.addEventListener('click', (e) => {
+      let currentButton = e.target;
+      if (currentButton.classList.contains('all')) {
+        showLista(listaAll);
+      }
+      if (currentButton.classList.contains('backend')) {
+        showLista(listaAll, "backend");
+      }
+      if (currentButton.classList.contains('data')) {
+        showLista(listaAll, "data");
+      }
+      if (currentButton.classList.contains('ia')) {
+        showLista(listaAll, "ia");
+      }
+      if (currentButton.classList.contains('all')) {
+        showLista(listaAll, "all");
+      }
+    });
   });
-  buttonGeral[index].classList.add('ativo');
-}
-
-buttonGeral.forEach((event, index) => {
-  event.addEventListener('click', () => {
-    removeClick(index);
-  });
-});
-
-buttonGeral.forEach((item) => {
-  item.addEventListener('click', (e) => {
-    let currentButton = e.target;
-    if (currentButton.classList.contains('all')) {
-      showLista(listaAll);
+  
+  function showLista(lista, button = "all") {
+  
+    lista.forEach((item) => {
+      item.classList.remove('ativo');
+    })
+    // Para editar os projetos, leve em consideração a ordem dos itens no html
+    if (button == 'backend') {
+      lista[0].classList.add('ativo');
+      lista[1].classList.add('ativo');
+      lista[2].classList.add('ativo');
+      lista[3].classList.add('ativo');
     }
-    if (currentButton.classList.contains('backend')) {
-      showLista(listaAll, "backend");
+    if (button == 'data') {
+      lista[4].classList.add('ativo');
+      lista[5].classList.add('ativo');
     }
-    if (currentButton.classList.contains('data')) {
-      showLista(listaAll, "data");
+    if (button == 'ia') {
+      lista[6].classList.add('ativo');
+      lista[7].classList.add('ativo');
     }
-    if (currentButton.classList.contains('ia')) {
-      showLista(listaAll, "ia");
-    }
-    if (currentButton.classList.contains('all')) {
-      showLista(listaAll, "all");
-    }
-  });
-});
-
-function showLista(lista, button = "all") {
-
-  lista.forEach((item) => {
-    item.classList.remove('ativo');
-  })
-  // Para editar os projetos, leve em consideração a ordem dos itens no html
-  if (button == 'backend') {
-    lista[0].classList.add('ativo');
-    lista[1].classList.add('ativo');
-    lista[2].classList.add('ativo');
-    lista[3].classList.add('ativo');
-  }
-  if (button == 'data') {
-    lista[4].classList.add('ativo');
-    lista[5].classList.add('ativo');
-  }
-  if (button == 'ia') {
-    lista[6].classList.add('ativo');
-    lista[7].classList.add('ativo');
-  }
-  if (button == 'all') {
-    for (let i = 0; i < lista.length; i++) {
-      lista[i].classList.add('ativo');
+    if (button == 'all') {
+      for (let i = 0; i < lista.length; i++) {
+        lista[i].classList.add('ativo');
+      }
     }
   }
 }
 
+gridProjects();
 digitandoTitulo();
 menuMobile();
 sobreMim();
